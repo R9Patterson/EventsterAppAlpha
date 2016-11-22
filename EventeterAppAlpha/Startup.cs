6c0d8b24bd48de9fed5e729e34b6125466da76aa
Ryan Patterson <ryanspatterson.rp@gmail.com>
2016-11-22 07:48:20 -0600

    Initial commit
    Finish the site!!! by 2pm 11/22/16

---
+++ EventeterAppAlpha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventeterAppAlpha.Startup))]
+namespace EventeterAppAlpha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
